2c718148005dd66aad222a4a304dcb82ff8d3a7e
sanjit2194 <kumar.sanjit211994@gmail.com>
2020-10-11 00:57:23 +0530

    feat: portfolio 1

---
+++ build/precache-manifest.9913dfe60aa7ec82febabf33851e05c4.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faf5136e8dbf32fc9a834e49b99db527",
+    "url": "/sanjit2194/dev-sanjit/index.html"
+  },
+  {
+    "revision": "ac1965138a4bcee74fab",
+    "url": "/sanjit2194/dev-sanjit/static/css/main.4dc86193.chunk.css"
+  },
+  {
+    "revision": "f66a5b026bebbbbb6c26",
+    "url": "/sanjit2194/dev-sanjit/static/js/2.92aab1d9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sanjit2194/dev-sanjit/static/js/2.92aab1d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac1965138a4bcee74fab",
+    "url": "/sanjit2194/dev-sanjit/static/js/main.774e868f.chunk.js"
+  },
+  {
+    "revision": "de737d81d418f49d63d6",
+    "url": "/sanjit2194/dev-sanjit/static/js/runtime-main.b31dcb09.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/sanjit2194/dev-sanjit/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/sanjit2194/dev-sanjit/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "6db903424bce8dbcd336fd093e4082b5",
+    "url": "/sanjit2194/dev-sanjit/static/media/dav.6db90342.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/sanjit2194/dev-sanjit/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "a8a5568a8a5ab789515f0fcab7ade658",
+    "url": "/sanjit2194/dev-sanjit/static/media/ics.a8a5568a.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/sanjit2194/dev-sanjit/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "bc5cf009732d4bd86dc6471a723c34c9",
+    "url": "/sanjit2194/dev-sanjit/static/media/profile.bc5cf009.jpg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/sanjit2194/dev-sanjit/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "c28b0f29be1ec8edd94de876a28e7e18",
+    "url": "/sanjit2194/dev-sanjit/static/media/soa.c28b0f29.png"
+  },
+  {
+    "revision": "c2b3e365214e3f4775cc3eb1cb25a37a",
+    "url": "/sanjit2194/dev-sanjit/static/media/telus.c2b3e365.jpg"
+  }
+]);
