e4f3b4d0b309ec406b6f20a27f704847f6188b1e
sanjit1994-telus <62537919+sanjit1994-telus@users.noreply.github.com>
2020-10-11 10:44:08 +0530

    fix: added pusblished

---
+++ build/precache-manifest.da29877f9b061612b0f8cbd1fe6204cd.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e652cf69e859a431928fad466542c100",
+    "url": "/sanjit2194/dev-portfolio/index.html"
+  },
+  {
+    "revision": "a940b3f69a89ea583bab",
+    "url": "/sanjit2194/dev-portfolio/static/css/main.c59a7561.chunk.css"
+  },
+  {
+    "revision": "ebf09ee8c2571095c368",
+    "url": "/sanjit2194/dev-portfolio/static/js/2.2f3cd825.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sanjit2194/dev-portfolio/static/js/2.2f3cd825.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a940b3f69a89ea583bab",
+    "url": "/sanjit2194/dev-portfolio/static/js/main.dbdd07e4.chunk.js"
+  },
+  {
+    "revision": "2a2a18bc2412c9876650",
+    "url": "/sanjit2194/dev-portfolio/static/js/runtime-main.910d44dd.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/sanjit2194/dev-portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/sanjit2194/dev-portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "6db903424bce8dbcd336fd093e4082b5",
+    "url": "/sanjit2194/dev-portfolio/static/media/dav.6db90342.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/sanjit2194/dev-portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "a8a5568a8a5ab789515f0fcab7ade658",
+    "url": "/sanjit2194/dev-portfolio/static/media/ics.a8a5568a.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/sanjit2194/dev-portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "bc5cf009732d4bd86dc6471a723c34c9",
+    "url": "/sanjit2194/dev-portfolio/static/media/profile.bc5cf009.jpg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/sanjit2194/dev-portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "c28b0f29be1ec8edd94de876a28e7e18",
+    "url": "/sanjit2194/dev-portfolio/static/media/soa.c28b0f29.png"
+  },
+  {
+    "revision": "c2b3e365214e3f4775cc3eb1cb25a37a",
+    "url": "/sanjit2194/dev-portfolio/static/media/telus.c2b3e365.jpg"
+  }
+]);
