6cf23f6044cae65a0c2361c299a471d851d97bbb
sanjit2194 <kumar.sanjit211994@gmail.com>
2020-10-24 19:23:54 +0530

    Updates

---
+++ precache-manifest.e736786317d44a9a64a31f590bf94983.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e895a33105c99f0ed8d5945f04a13144",
+    "url": "/dev-portfolio/index.html"
+  },
+  {
+    "revision": "474ed7ee6c25640d5298",
+    "url": "/dev-portfolio/static/css/main.d2a218e3.chunk.css"
+  },
+  {
+    "revision": "345fc9e0e956c2cc7291",
+    "url": "/dev-portfolio/static/js/2.c3c156bc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dev-portfolio/static/js/2.c3c156bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "474ed7ee6c25640d5298",
+    "url": "/dev-portfolio/static/js/main.62f37681.chunk.js"
+  },
+  {
+    "revision": "e3425e8c6543e5fc1ee4",
+    "url": "/dev-portfolio/static/js/runtime-main.63f67cfb.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/dev-portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/dev-portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "6db903424bce8dbcd336fd093e4082b5",
+    "url": "/dev-portfolio/static/media/dav.6db90342.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/dev-portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "a8a5568a8a5ab789515f0fcab7ade658",
+    "url": "/dev-portfolio/static/media/ics.a8a5568a.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/dev-portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "bc5cf009732d4bd86dc6471a723c34c9",
+    "url": "/dev-portfolio/static/media/profile.bc5cf009.jpg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/dev-portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "c28b0f29be1ec8edd94de876a28e7e18",
+    "url": "/dev-portfolio/static/media/soa.c28b0f29.png"
+  },
+  {
+    "revision": "c2b3e365214e3f4775cc3eb1cb25a37a",
+    "url": "/dev-portfolio/static/media/telus.c2b3e365.jpg"
+  }
+]);
